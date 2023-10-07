98e47543b53b003bd8bbf52c4c27f73b1971b816
semon69 <smdemon3@gmail.com>
2023-10-08 00:42:39 +0600

    create a dummy data and display post item

--- src/components/PostSide/PostSide.jsx
+++ src/components/PostSide/PostSide.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import PostShare from './PostShare';
+import Posts from './Posts';
 
 const PostSide = () => {
     return (
         <div className=''>
             <PostShare></PostShare>
             <div>
-                
+                <Posts></Posts>
             </div>
         </div>
     );
--- src/components/PostSide/Posts.jsx
+++ src/components/PostSide/Posts.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import { AiOutlineHeart } from 'react-icons/ai';
+import { BiCommentDetail } from 'react-icons/bi';
+import { FiSend } from 'react-icons/fi';
 
 const Posts = () => {
     return (
-        <div>
-            <img src={'https://images.unsplash.com/photo-1492562080023-ab3db95bfbce?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTgyfHxwcm9maWxlJTIwcGljdHVyZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60'} alt="" />
-            <div>
-                <AiOutlineHeart />
+        <div className='p-4 rounded-md bg-zinc-100'>
+            <img className='object-cover w-full rounded-md max-h-96' src={'https://images.unsplash.com/photo-1492562080023-ab3db95bfbce?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTgyfHxwcm9maWxlJTIwcGljdHVyZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60'} alt="" />
+            <div className='flex gap-4 py-3 text-3xl'>
+                <div>
+                    <AiOutlineHeart />
+                </div>
+                <BiCommentDetail />
+                <FiSend />
             </div>
             <p>{"100 likes"}</p>
-            <div>
-                <h1>{'Nomuasu'}</h1>
+            <div className='flex gap-2'>
+                <h1 className='font-bold'>{'Nomuasu'}</h1>
                 <h1>{"Brand new photo of mine"}</h1>
             </div>
         </div>
